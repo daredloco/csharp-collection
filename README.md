9ec5d81e2a5e199b9746bc2d6d7532a7b3f5aa35
Roman Wanner <roman_wanner@rowa-digital.ch>
2020-01-24 03:45:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+# C Sharp Collection
+
+A collection of scripts for Unity
+
+
+## LoCa.cs
+
+### Usage:
+
+#### Initialize files
+##### Simple:
+```cs
+RoWa.LoCa.Init(LOCALIZATION_FILES_DIRECTORY);
+
+RoWa.LoCa.Init("/localization");
+```
+
+##### With default language:
+```cs
+RoWa.LoCa.Init(LOCALIZATION_FILES_DIRECTORY,DEFAULT_LANGUAGE);
+
+RoWa.LoCa.Init("/localization","en");
+```
+
+##### With extension:
+```cs
+RoWa.LoCa.Init(LOCALIZATION_FILES_DIRECTORY,DEFAULT_LANGUAGE,EXTENSION);
+RoWa.LoCa.Init("/localization","en",".txt");
+```
+
+#### Translate
+```cs
+RoWa.LoCa.Trans(KEY);
+
+RoWa.LoCa.Trans("word");
+
+RoWa.LoCa.Translate("word");
+```
+
+#### Set languages
+##### Default:
+```
+Must be set at the RoWa.LoCa.Init() function!
+```
+
+##### User:
+```cs
+UserLanguage = Languages[KEY];
+
+UserLanguage = Languages["en"];
+```
+
+### Roadmap
+- [x] Loading the languages
+- [x] Translate the keys
+- [ ] Set the default language (Can only be set on Init() at the moment)
+- [ ] Set the user language (Will be set same as the default language)
---
+++ src/LoCa.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RoWa
+{
+		public static class LoCa
+		{
+		public static Dictionary<string, Language> Languages { get; private set; }
+		public static Language UserLanguage { get; private set; }
+
+		public static void Init(string dir, string defaultlanguage = "en", string extension = ".txt")
+		{
+			if (!Directory.Exists(dir))
+				throw new LoCaException("Directory '" + dir + "' does not exist!");
+
+			foreach(string fname in Directory.GetFiles(dir))
+			{
+				if(new FileInfo(fname).Extension == extension)
+				{
+					Language lang = new Language(fname);
+					Languages.Add(lang.key, lang);
+				}
+			}
+
+			if (!Languages.ContainsKey(defaultlanguage))
+				throw new Exception("Couldn't find default language '" + defaultlanguage + "'!");
+
+			UserLanguage = Languages[defaultlanguage];
+		}
+
+		public static void Trans(string key)
+		{
+			if(UserLanguage == null)
+			{
+				throw new LoCaException("No UserLanguage set!");
+			}
+			UserLanguage.Trans(key);
+		}
+
+		public static void Translate(string key)
+		{
+			Trans(key);
+		}
+
+		public class Language
+		{
+			public string key { get; private set; }
+			public string english { get; private set; }
+			public string local { get; private set; }
+			public Dictionary<string,string> dict { get; private set; }
+
+			public Language(string fname)
+			{
+				if (!File.Exists(fname))
+					throw new LoCaException("Languagefile '" + fname + "' couldn't be found!");
+
+				int lcount = 0;
+				foreach(string fline in File.ReadAllLines(fname))
+				{
+					lcount++;
+					if (fline.StartsWith("language_key="))					
+						key = fline.Replace("language_key=", "");
+					else if (fline.StartsWith("language_english"))
+						english = fline.Replace("language_english", "");
+					else if (fline.StartsWith("language_local"))
+						local = fline.Replace("language_local", "");
+					else if(fline.StartsWith("#") || fline == "" || !fline.Contains("="))
+					{                       
+						//Do nothing...
+					}
+					else
+					{
+						string k = fline.Split('=')[0];
+						string v = fline.Replace(k + "=", "");
+						if (dict.ContainsKey(k))
+							throw new LoCaException("Error on line " + lcount + ": Key '" + k + "' does already exist inside file '" + fname + "'!");
+
+						dict.Add(k, v);
+					}
+				}
+				if(key == null)
+					throw new LoCaException("Error inside file '" + fname + "': Key 'language_key' isn't set!");
+				if(english == null)
+					throw new LoCaException("Error inside file '" + fname + "': Key 'language_english' isn't set!");
+				if (local == null)
+					throw new LoCaException("Error inside file '" + fname + "': Key 'language_local' isn't set!");
+			}
+
+			public string Trans(string key)
+			{
+				if (dict.ContainsKey(key))
+					return dict[key];
+
+				return "{" + key + "}";
+			}
+
+			public string Translate(string key)
+			{
+				return Trans(key);
+			}
+			}
+
+			public class LoCaException : Exception
+			{
+				public LoCaException()
+				{
+				}
+
+				public LoCaException(string message)
+					: base(message)
+				{
+				}
+
+				public LoCaException(string message, Exception inner)
+					: base(message, inner)
+				{
+				}
+			}
+		}
+}
